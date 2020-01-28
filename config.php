03d0eb8f114e1ff10e80c728e7d1dff80ce55fca
cfd2000 <52366440+cfd2000@users.noreply.github.com>
2020-01-29 01:06:59 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?
+
+$UserAgent = "Mozilla/5.0 (Linux; Android 9; CPH1911) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.68 Mobile Safari/537.36";
+$username = "Ustadznenen";
+$id = "140624";
+$PHPSESSID = "24b2d5078801ae91cc792fa1c152ed64";
